d78fe14da0a636718f474b602c4747ec88bed87a
gabbiurlaro <iurlaro.gabriele@gmail.com>
2023-01-19 17:20:08 +0100

    ActionNet dataset

--- save_feat.py
+++ save_feat.py
@@ -3,7 +3,7 @@ from utils.logger import logger
 import torch.nn.parallel
 import torch.optim
 import torch
-from utils.loaders import EpicKitchensDataset
+from utils.loaders import EpicKitchensDataset, ActionNetDataset
 from utils.args import args
 from utils.utils import pformat_dict
 import utils
@@ -59,7 +59,7 @@ def main():
     if args.action == "save":
         augmentations = {"train": train_augmentations, "test": test_augmentations}
         # the only action possible with this script is "save"
-        loader = torch.utils.data.DataLoader(EpicKitchensDataset(args.dataset.shift.split("-")[1], modalities,
+        loader = torch.utils.data.DataLoader(ActionNetDataset(args.dataset.shift.split("-")[1], modalities,
                                                                  args.split, args.dataset,
                                                                  args.save.num_frames_per_clip,
                                                                  args.save.num_clips, args.save.dense_sampling,
