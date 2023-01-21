68a2f9798a7fba4cc2e47645f0869f8abb9e7f95
gabbiurlaro <iurlaro.gabriele@gmail.com>
2023-01-21 19:35:10 +0100

    Modified models list to retrieve trn submodules

--- models/__init__.py
+++ models/__init__.py
@@ -1,4 +1,4 @@
-from models.TRN import *
+from models.TRN import RelationModuleMultiScale, RelationModuleMultiScaleWithClassifier
 from models.VideoModel import VideoModel
 from models.I3D import I3D
 from models.FinalClassifier import Classifier, MLP_late_fusion, action_TRN
--- test_TRN.py
+++ test_TRN.py
@@ -1,16 +1,16 @@
-from models import TRN
+from models import RelationModuleMultiScale
 
 import torch
 from torch.autograd import Variable
 
 if __name__ == "__main__":
-    batch_size = 10
+    batch_size = 32
     num_frames = 5
     num_class = 8
-    img_feature_dim = 512
+    img_feature_dim = 1024
     print(f"Feeding the TRN with a batch of dimension {batch_size}, with {num_frames} and a feature dimension of {img_feature_dim}")
     input_var = Variable(torch.randn(batch_size, num_frames, img_feature_dim))
-    model = TRN(img_feature_dim, num_frames, num_class)
+    model = RelationModuleMultiScale(img_feature_dim, num_frames, num_class)
     output = model(input_var)
     print(f"output: {output}\n\nSize: {output.size()}")
 
