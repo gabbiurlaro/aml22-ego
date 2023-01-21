9bbf4215cf278a57755093aec64e986eed6e2162
Salvatore Adalberto Esposito <salvatore@MacBook-Air-di-Salvatore.local>
2023-01-21 14:09:02 +0100

    second try classifier

--- models/__init__.py
+++ models/__init__.py
@@ -1,4 +1,3 @@
 from models.VideoModel import VideoModel
 from models.I3D import I3D
 from models.FinalClassifier import Classifier, MLP_late_fusion, action_TRN
-from models.TRN import RelationModuleMultiScaleWithClassifier as TRN
