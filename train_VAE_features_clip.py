794f3234cfd868b6c4c062027cf2f6a4cc8c9e7a
Salvatore Adalberto Esposito <salvatore@MacBook-Air-di-Salvatore.local>
2023-01-28 18:34:26 +0100

    second VaE

--- train_VAE_features_clip.py
+++ train_VAE_features_clip.py
@@ -127,13 +127,14 @@ def plot_latent(autoencoder, data, device, num_batches=100):
             #     plt.colorbar()
             #     break
     #plt.show()
-    filtered = {}
-    ue['x'] = reduced[:, 0]
-    ue['y'] = reduced[:, 1]
-    for i in range(8): # ek has 8 classes
-        filtered['x'] = [ue['x'][j]  for j, out in enumerate(Y) if out==i ]
-        filtered['y'] = [ue['y'][j]  for j, out in enumerate(Y) if out==i ]
-        plt.scatter(filtered['x'], filtered['y'], c=Y[i], label=Y[i])
+    # filtered = {}
+    # ue['x'] = reduced[:, 0]
+    # ue['y'] = reduced[:, 1]
+    # for i in range(8): # ek has 8 classes
+    #     filtered['x'] = [ue['x'][j]  for j, out in enumerate(Y) if out==i ]
+    #     filtered['y'] = [ue['y'][j]  for j, out in enumerate(Y) if out==i ]
+    #     plt.scatter(filtered['x'], filtered['y'], c=Y[i], label=Y[i])
+    plt.scatter(reduced[:,0], reduced[:,1], c=Y, label=Y)
     plt.legend()
     #plt.title(title)
     plt.savefig(f"img_VAE.png")
