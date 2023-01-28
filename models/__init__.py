1e1fb96eb738e6886fd9171107c15961742ba894
Salvatore Adalberto Esposito <salvatore@MacBook-Air-di-Salvatore.local>
2023-01-28 11:43:38 +0100

    first Vae try

--- models/__init__.py
+++ models/__init__.py
@@ -2,5 +2,5 @@ from models.TRN import *
 from models.VideoModel import VideoModel
 from models.I3D import I3D
 from models.FinalClassifier import Classifier, MLP_late_fusion, action_TRN
-from models.VAE import VAE, Encoder, Decoder
+from models.VAE_linear import VariationalEncoder, Decoder, VariationalAutoencoder
 
--- train_VAE_features_clip.py
+++ train_VAE_features_clip.py
@@ -62,21 +62,19 @@ def main():
         # notice that here, the first parameter passed is the input dimension
         # In our case it represents the feature dimensionality which is equivalent to 1024 for I3D
         #print(getattr(model_list, args.models[m].model)())
-        models[m] = getattr(model_list, args.models[m].model)(1024, 2, num_classes)
+        models[m] = getattr(model_list, args.models[m].model)(1024, 2, 1024)
 
     # the models are wrapped into the ActionRecognition task which manages all the training steps
-    action_classifier = tasks.ActionRecognition("action-classifier", models, args.batch_size,
-                                                args.total_batch, args.models_dir, num_classes,
-                                                args.train.num_clips, args.models, args=args)
-    action_classifier.load_on_gpu(device)
-
+    # action_classifier = tasks.ActionRecognition("action-classifier", models, args.batch_size,
+    #                                             args.total_batch, args.models_dir, num_classes,
+    #   
     if args.action == "train":
         # resume_from argument is adopted in case of restoring from a checkpoint
-        if args.resume_from is not None:
-            action_classifier.load_last_model(args.resume_from)
+        # if args.resume_from is not None:
+        #     action_classifier.load_last_model(args.resume_from)
         # i.e. number of batches passed
         # notice, here it is multiplied by tot_batch/batch_size since gradient accumulation technique is adopted
-        training_iterations = args.train.num_iter * (args.total_batch // args.batch_size)
+        # training_iterations = args.train.num_iter * (args.total_batch // args.batch_size)
         # all dataloaders are generated here
         train_loader = torch.utils.data.DataLoader(EpicKitchensDataset(args.dataset.shift.split("-")[0], modalities,
                                                                        'train', args.dataset, None, None, None,
@@ -89,172 +87,21 @@ def main():
                                                                      None, load_feat=True),
                                                  batch_size=args.batch_size, shuffle=False,
                                                  num_workers=args.dataset.workers, pin_memory=True, drop_last=False)
-        train(action_classifier, train_loader, val_loader, device, num_classes)
-
-    elif args.action == "validate":
-        if args.resume_from is not None:
-            action_classifier.load_last_model(args.resume_from)
-        val_loader = torch.utils.data.DataLoader(EpicKitchensDataset(args.dataset.shift.split("-")[-1], modalities,
-                                                                     'val', args.dataset, None, None, None,
-                                                                     None, load_feat=True),
-                                                 batch_size=args.batch_size, shuffle=False,
-                                                 num_workers=args.dataset.workers, pin_memory=True, drop_last=False)
-
-        validate(action_classifier, val_loader, device, action_classifier.current_iter, num_classes)
-
-
-def train(action_classifier, train_loader, val_loader, device, num_classes):
-    """
-    function to train the model on the test set
-    action_classifier: Task containing the model to be trained
-    train_loader: dataloader containing the training data
-    val_loader: dataloader containing the validation data
-    device: device on which you want to test
-    num_classes: int, number of classes in the classification problem
-    """
-    global training_iterations, modalities
-
-    data_loader_source = iter(train_loader)
-    action_classifier.train(True)
-    action_classifier.zero_grad()
-    iteration = action_classifier.current_iter * (args.total_batch // args.batch_size)
-
-    # the batch size should be total_batch but batch accumulation is done with batch size = batch_size.
-    # real_iter is the number of iterations if the batch size was really total_batch
-    for i in range(iteration, training_iterations):
-        # iteration w.r.t. the paper (w.r.t the bs to simulate).... i is the iteration with the actual bs( < tot_bs)
-        real_iter = (i + 1) / (args.total_batch // args.batch_size)
-        if real_iter == args.train.lr_steps:
-            # learning rate decay at iteration = lr_steps
-            action_classifier.reduce_learning_rate()
-        # gradient_accumulation_step is a bool used to understand if we accumulated at least total_batch
-        # samples' gradient
-        gradient_accumulation_step = real_iter.is_integer()
-
-        """
-        Retrieve the data from the loaders
-        """
-        start_t = datetime.now()
-        # the following code is necessary as we do not reason in epochs so as soon as the dataloader is finished we need
-        # to redefine the iterator
-        try:
-            source_data, source_label = next(data_loader_source)
-        except StopIteration:
-            data_loader_source = iter(train_loader)
-            source_data, source_label = next(data_loader_source)
-        end_t = datetime.now()
-
-        logger.info(f"Iteration {i}/{training_iterations} batch retrieved! Elapsed time = "
-                    f"{(end_t - start_t).total_seconds() // 60} m {(end_t - start_t).total_seconds() % 60} s")
-
-        ''' Action recognition'''
-        source_label = source_label.to(device)
-        data = {}
-        logits = []
-
-        # for clip in range(args.train.num_clips):
-            # in case of multi-clip training one clip per time is processed
-        for m in modalities:
-            data[m] = source_data[m].to(device)
-            data[m] = torch.reshape(data[m], (5,32,1024)) #to be uncommented for late fusion
-           
-        logits, _ = action_classifier.forward(data)
-        
-        action_classifier.compute_loss(logits, source_label, loss_weight=1)
-        action_classifier.backward(retain_graph=False)
-        action_classifier.compute_accuracy(logits, source_label)
-
-        action_classifier.wandb_log()
-
-        # update weights and zero gradients if total_batch samples are passed
-        if gradient_accumulation_step:
-            logger.info("[%d/%d]\tlast Verb loss: %.4f\tMean verb loss: %.4f\tAcc@1: %.2f%%\tAccMean@1: %.2f%%" %
-                        (real_iter, args.train.num_iter, action_classifier.loss.val, action_classifier.loss.avg,
-                         action_classifier.accuracy.val[1], action_classifier.accuracy.avg[1]))
-
-            action_classifier.check_grad()
-            action_classifier.step()
-            action_classifier.zero_grad()
-
-        # every eval_freq "real iteration" (iterations on total_batch) the validation is done, notice we validate and
-        # save the last 9 models
-        if gradient_accumulation_step and real_iter % args.train.eval_freq == 0:
-            val_metrics = validate(action_classifier, val_loader, device, int(real_iter), num_classes)
-            wandb.log({'accuracy on val': val_metrics['top1']})
-            if val_metrics['top1'] <= action_classifier.best_iter_score:
-                logger.info("New best accuracy {:.2f}%"
-                            .format(action_classifier.best_iter_score))
-            else:
-                logger.info("New best accuracy {:.2f}%".format(val_metrics['top1']))
-                action_classifier.best_iter = real_iter
-                action_classifier.best_iter_score = val_metrics['top1']
-
-            action_classifier.save_model(real_iter, val_metrics['top1'], prefix=None)
-            action_classifier.train(True)
-
-
-def validate(model, val_loader, device, it, num_classes):
-    """
-    function to validate the model on the test set
-    model: Task containing the model to be tested
-    val_loader: dataloader containing the validation data
-    device: device on which you want to test
-    it: int, iteration among the training num_iter at which the model is tested
-    num_classes: int, number of classes in the classification problem
-    """
-    global modalities
-
-    model.reset_acc()
-    model.train(False)
-    logits = {}
-
-    # Iterate over the models
-    with torch.no_grad():
-        for i_val, (data, label) in enumerate(val_loader):
-            label = label.to(device)
-
-            for m in modalities:
-                batch = data[m].shape[0]
-                logits[m] = torch.zeros((args.test.num_clips, batch, num_classes)).to(device)
-
-            clip = {}
-            for i_c in range(args.test.num_clips):
-                for m in modalities:
-                    clip[m] = data[m][:, i_c].to(device)
-
-                output, _ = model(clip)
-                for m in modalities:
-                    logits[m][i_c] = output[m]
-
-            for m in modalities:
-                logits[m] = torch.mean(logits[m], dim=0)
-
-            model.compute_accuracy(logits, label)
-
-            if (i_val + 1) % (len(val_loader) // 5) == 0:
-                logger.info("[{}/{}] top1= {:.3f}% top5 = {:.3f}%".format(i_val + 1, len(val_loader),
-                                                                          model.accuracy.avg[1], model.accuracy.avg[5]))
-
-        class_accuracies = [(x / y) * 100 for x, y in zip(model.accuracy.correct, model.accuracy.total)]
-        logger.info('Final accuracy: top1 = %.2f%%\ttop5 = %.2f%%' % (model.accuracy.avg[1],
-                                                                      model.accuracy.avg[5]))
-        for i_class, class_acc in enumerate(class_accuracies):
-            logger.info('Class %d = [%d/%d] = %.2f%%' % (i_class,
-                                                         int(model.accuracy.correct[i_class]),
-                                                         int(model.accuracy.total[i_class]),
-                                                         class_acc))
-
-    logger.info('Accuracy by averaging class accuracies (same weight for each class): {}%'
-                .format(np.array(class_accuracies).mean(axis=0)))
-    test_results = {'top1': model.accuracy.avg[1], 'top5': model.accuracy.avg[5],
-                    'class_accuracies': np.array(class_accuracies)}
-
-    with open(os.path.join(args.log_dir, f'val_precision_{args.dataset.shift.split("-")[0]}-'
-                                         f'{args.dataset.shift.split("-")[-1]}.txt'), 'a+') as f:
-        f.write("[%d/%d]\tAcc@top1: %.2f%%\n" % (it, args.train.num_iter, test_results['top1']))
-
-    return test_results
-
+       # train(action_classifier, train_loader, val_loader, device, num_classes)
+        train(models['RGB'], train_loader, device)
+    
+def train(autoencoder, data, device, epochs=20):
+    opt = torch.optim.Adam(autoencoder.parameters())
+    for epoch in range(epochs):
+        for x, y in data:
+            x = x.to(device) # GPU
+            opt.zero_grad()
+            x_hat = autoencoder(x)
+            loss = ((x - x_hat)**2).sum() + autoencoder.encoder.kl
+            loss.backward()
+            opt.step()
+            wandb.log({'log_loss': loss})
+    return autoencoder
 
 if __name__ == '__main__':
     main()
