0a0d7028720620db4b685e92657e42523466089f
Salvatore Adalberto Esposito <salvatore@MacBook-Air-di-Salvatore.local>
2023-01-28 18:43:22 +0100

    second VaE

--- train_VAE_features_clip.py
+++ train_VAE_features_clip.py
@@ -134,6 +134,7 @@ def plot_latent(autoencoder, data, device, num_batches=100):
     #     filtered['x'] = [ue['x'][j]  for j, out in enumerate(Y) if out==i ]
     #     filtered['y'] = [ue['y'][j]  for j, out in enumerate(Y) if out==i ]
     #     plt.scatter(filtered['x'], filtered['y'], c=Y[i], label=Y[i])
+    latent =  np.array(latent).ravel()
     plt.scatter(latent[:,0], latent[:,1], c=Y, label=Y)
     plt.legend()
     #plt.title(title)
