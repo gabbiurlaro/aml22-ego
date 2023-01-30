23b2476c074a09628bb1b0029e09bb59c13992c3
Salvatore Adalberto Esposito <salvatore@MacBook-Air-di-Salvatore.local>
2023-01-30 11:59:26 +0100

    training longer VAE

--- train_VAE_features_clip.py
+++ train_VAE_features_clip.py
@@ -97,7 +97,7 @@ def main():
 def train(autoencoder, train_dataloader, device, epochs=200):
     for m in modalities:
         autoencoder[m].load_on(device)
-    opt = torch.optim.Adam(autoencoder['RGB'].parameters(), lr=0.00001)
+    opt = torch.optim.SGD(autoencoder['RGB'].parameters(), lr=0.00001)
 
     losses = []
     for epoch in range(epochs):
