6096aca67c416da84d3f10a093e27fbedf630ef5
Salvatore Adalberto Esposito <salvatore@MacBook-Air-di-Salvatore.local>
2023-01-31 17:54:51 +0100

    VAE_ RGB

--- train_VAE_features_clip.py
+++ train_VAE_features_clip.py
@@ -246,7 +246,7 @@ def plot_latent(autoencoder, dataloader, device, split = 'train'):
 
 def load_model(ae, path):
     state_dict = torch.load(path)["model_state_dict"]
-    print(state_dict)
+    print([x for x in state_dict.keys()])
     ae.load_state_dict(state_dict, strict=False)
 
 
