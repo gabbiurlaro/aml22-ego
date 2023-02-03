f20d4c87d7e8c978a86aefcdd0257a34228fd0c2
SalvatoreAdalberto <salvatoreadalberto.esposito@gmail.com>
2023-02-03 11:58:14 +0100

    update EMG_VAE

--- utils/loaders.py
+++ utils/loaders.py
@@ -496,7 +496,7 @@ class ActionNetDataset(data.Dataset, ABC):
                 freq[arm] = [spectrogram(signal[:, i]) for i in range(8)]
                 for channel in freq[arm]:
                     spec_indices = [int(i/30*160) for i in indices]
-                    print(f"channel : {channel.shape} spec_iindices: {spec_indices}")
+                    print(f"channel : {channel.shape} spec_indices: {len(spec_indices)}")
                     result.append(torch.stack([channel[:, i] for i in spec_indices]))
             result = torch.stack(result)
             return result, record.label
