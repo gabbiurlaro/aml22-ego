0882185d5a3765ebd738abfcd960a1143fdae319
SalvatoreAdalberto <salvatoreadalberto.esposito@gmail.com>
2023-02-03 15:10:04 +0100

    update EMG_VAE

--- train_VAE_EMG.py
+++ train_VAE_EMG.py
@@ -199,6 +199,7 @@ def train(autoencoder, train_dataloader, val_dataloader, device, model_args):
         for i, (data, labels) in enumerate(train_dataloader):
             opt.zero_grad()
             for m in modalities:
+                print(data[m])
                 data[m] = data[m].permute(1, 0, 2)
                 # print(f"Data after permutation: {data[m].size()}")
             for i_c in range(args.test.num_clips):
