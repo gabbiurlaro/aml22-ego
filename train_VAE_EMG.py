9d1b8023c4829de65424af66c433254aaee0b8d4
SalvatoreAdalberto <salvatoreadalberto.esposito@gmail.com>
2023-02-04 11:48:33 +0100

    update EMG_VAE

--- train_VAE_EMG.py
+++ train_VAE_EMG.py
@@ -80,13 +80,13 @@ def main():
         # training_iterations = args.train.num_iter * (args.total_batch // args.batch_size)
         # all dataloaders are generated here
         train_loader = torch.utils.data.DataLoader(ActionNetDataset(args.dataset.shift.split("-")[0], modalities,
-                                                                       'train', args.dataset, {'EMG': 32}, 5, {'EMG': False},
+                                                                       'train', args.dataset, {'EMG': 16}, 5, {'EMG': True},
                                                                        None, load_feat=False),
                                                    batch_size=args.batch_size, shuffle=True,
                                                    num_workers=args.dataset.workers, pin_memory=True, drop_last=True)
 
         val_loader = torch.utils.data.DataLoader(ActionNetDataset(args.dataset.shift.split("-")[-1], modalities,
-                                                                     'test', args.dataset,  {'EMG': 32}, 5, {'EMG': False},
+                                                                     'test', args.dataset,  {'EMG': 16}, 5, {'EMG': True},
                                                                      None, load_feat=False),
                                                  batch_size=args.batch_size, shuffle=False,
                                                  num_workers=args.dataset.workers, pin_memory=True, drop_last=False)
