6f044c6a641176a3aa887d2a9c27f5ba85d13f79
SalvatoreAdalberto <salvatoreadalberto.esposito@gmail.com>
2023-05-13 19:11:37 +0200

    no sweeps

--- tasks/action_recognition_task.py
+++ tasks/action_recognition_task.py
@@ -12,7 +12,7 @@ class ActionRecognition(tasks.Task, ABC):
     optimizer = {}
 
     def __init__(self, name, task_models, batch_size, total_batch, models_dir, num_classes,
-                 num_clips, model_args, args, wandb=None, **kwargs) -> None:
+                 num_clips, model_args, args, wandb=None,  device=None, **kwargs) -> None:
         super().__init__(name, task_models, batch_size, total_batch, models_dir, args, **kwargs)
         # Accuracy measures
         self.model_args = model_args
@@ -22,7 +22,10 @@ class ActionRecognition(tasks.Task, ABC):
         self.criterion = torch.nn.CrossEntropyLoss(weight=None, size_average=None, ignore_index=-100,
                                                    reduce=None, reduction='none')
         optim_params = {}
+
         for m in self.modalities:
+            if device:
+                self.task_models[m].to(self.device)
             optim_params[m] = filter(lambda parameter: parameter.requires_grad, self.task_models[m].parameters())
             self.optimizer[m] = torch.optim.SGD(optim_params[m], (wandb.lr if wandb else model_args[m].lr),
                                                 weight_decay=(wandb.weight_decay if wandb else model_args[m].weight_decay),
