c9f1afc949b5b6ef468e9609fda5807b5c16d992
SalvatoreAdalberto <salvatoreadalberto.esposito@gmail.com>
2023-05-13 17:09:52 +0200

    new sweeps

--- train_classifier_EMG.py
+++ train_classifier_EMG.py
@@ -35,7 +35,7 @@ def init_operations():
     # wanbd logging configuration
     
     if args.wandb_name is not None:
-        wandb.login(key='c87fa53083814af2a9d0ed46e5a562b9a5f8b3ec')
+        #wandb.login(key='c87fa53083814af2a9d0ed46e5a562b9a5f8b3ec')
         wandb.init()
         # wandb.run.name = args.name + "_" + args.shift.split("-")[0] + "_" + args.shift.split("-")[-1]
         # wandb.run.name = f'{args.name}_{args.models.RGB.model}'
