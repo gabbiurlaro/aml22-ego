27c6b5e37a014eb97715a27ac3c42abc26434d7e
SalvatoreAdalberto <salvatoreadalberto.esposito@gmail.com>
2023-05-13 15:53:21 +0200

    inchannells

--- models/EMG_classifier.py
+++ models/EMG_classifier.py
@@ -10,19 +10,25 @@ class EMG_classifier(nn.Module):
         self.num_clips = num_clips
         self.classifier = nn.Sequential(
             nn.Conv2d(num_input, 32, kernel_size=4, stride=2, padding=1),
+            nn.BatchNorm2d(32),
             nn.ReLU(),
             nn.Conv2d(32, 64, kernel_size=4, stride=2, padding=1),
+            nn.BatchNorm2d(64),
             nn.ReLU(),
             nn.Conv2d(64, 128, kernel_size=4, stride=2, padding=1),
+            nn.BatchNorm2d(128),
             nn.ReLU(),
             nn.Conv2d(128, 256, kernel_size=4, stride=2, padding=1),
+            nn.BatchNorm2d(256),
             nn.ReLU(),
             nn.Dropout(p=0.1),
             nn.MaxPool2d(2, stride=1)
         )
         self.fc = nn.Sequential(
             nn.Linear(256, 128),
-            nn.Linear(128, num_classes)
+            nn.ReLU(),
+            nn.Linear(128, num_classes),
+            nn.Softmax(dim=num_classes)
         )
 
     def forward(self, x):
