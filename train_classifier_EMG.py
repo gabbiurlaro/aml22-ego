3fee5f197268fe010808b7c70d58e13db9be8589
SalvatoreAdalberto <salvatoreadalberto.esposito@gmail.com>
2023-05-13 15:50:26 +0200

    inchannells

--- train_classifier_EMG.py
+++ train_classifier_EMG.py
@@ -246,19 +246,18 @@ def validate(model, val_loader, device, it, num_classes):
                 logger.info("[{}/{}] top1= {:.3f}% top5 = {:.3f}%".format(i_val + 1, len(val_loader),
                                                                           model.accuracy.avg[1], model.accuracy.avg[5]))
 
-        class_accuracies = [(x / y) * 100 for x, y in zip(model.accuracy.correct, model.accuracy.total)]
+        # class_accuracies = [(x / y) * 100 for x, y in zip(model.accuracy.correct, model.accuracy.total)]
         logger.info('Final accuracy: top1 = %.2f%%\ttop5 = %.2f%%' % (model.accuracy.avg[1],
                                                                       model.accuracy.avg[5]))
-        for i_class, class_acc in enumerate(class_accuracies):
-            logger.info('Class %d = [%d/%d] = %.2f%%' % (i_class,
-                                                         int(model.accuracy.correct[i_class]),
-                                                         int(model.accuracy.total[i_class]),
-                                                         class_acc))
+        # for i_class, class_acc in enumerate(class_accuracies):
+        #     logger.info('Class %d = [%d/%d] = %.2f%%' % (i_class,
+        #                                                  int(model.accuracy.correct[i_class]),
+        #                                                  int(model.accuracy.total[i_class]),
+        #                                                  class_acc))
 
-    logger.info('Accuracy by averaging class accuracies (same weight for each class): {}%'
-                .format(np.array(class_accuracies).mean(axis=0)))
-    test_results = {'top1': model.accuracy.avg[1], 'top5': model.accuracy.avg[5],
-                    'class_accuracies': np.array(class_accuracies)}
+    # logger.info('Accuracy by averaging class accuracies (same weight for each class): {}%'
+    #             .format(np.array(class_accuracies).mean(axis=0)))
+    test_results = {'top1': model.accuracy.avg[1], 'top5': model.accuracy.avg[5]}
 
     with open(os.path.join(args.log_dir, f'val_precision_{args.dataset.shift.split("-")[0]}-'
                                          f'{args.dataset.shift.split("-")[-1]}.txt'), 'a+') as f:
