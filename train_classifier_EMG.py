92c1277ea8d26d9e54e82aca454adedba132bf11
SalvatoreAdalberto <salvatoreadalberto.esposito@gmail.com>
2023-05-13 16:37:37 +0200

    new sweeps

--- train_classifier_EMG.py
+++ train_classifier_EMG.py
@@ -36,9 +36,9 @@ def init_operations():
     
     if args.wandb_name is not None:
         wandb.login(key='c87fa53083814af2a9d0ed46e5a562b9a5f8b3ec')
-        #wandb.init(project="test-project", entity="egovision-aml22")
-        #wandb.run.name = args.name + "_" + args.shift.split("-")[0] + "_" + args.shift.split("-")[-1]
-        #wandb.run.name = f'{args.name}_{args.models.RGB.model}'
+        wandb.init(project="test-project", entity="egovision-aml22")
+        wandb.run.name = args.name + "_" + args.shift.split("-")[0] + "_" + args.shift.split("-")[-1]
+        wandb.run.name = f'{args.name}_{args.models.RGB.model}'
 
 
 
