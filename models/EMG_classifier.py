84b9080c695adb1221812df6700e192b9bbc7737
SalvatoreAdalberto <salvatoreadalberto.esposito@gmail.com>
2023-05-13 12:59:53 +0200

    inchannells

--- models/EMG_classifier.py
+++ models/EMG_classifier.py
@@ -17,7 +17,9 @@ class EMG_classifier(nn.Module):
             nn.ReLU(),
             nn.Conv2d(128, 256, kernel_size=4, stride=2, padding=1),
             nn.ReLU(),
-            nn.Dropout(p=0.6),
+            nn.Dropout(p=0.6)
+        )
+        self.fc = nn.Sequential(
             nn.MaxPool2d(2),
             nn.Linear(256, 128),
             nn.Linear(128, num_classes)
@@ -26,6 +28,9 @@ class EMG_classifier(nn.Module):
     def forward(self, x):
         logits = []
         for clip in range(self.num_clips):
-            logits.append(self.classifier(x[clip,:]))
+            y = self.classifier(x[clip,:])
+            print(f'y shape: {y.shape}')
+            exit(-1)
+            logits.append(self.fc(y))
 
         return torch.stack(logits, dim=0).mean(dim=0), {}
