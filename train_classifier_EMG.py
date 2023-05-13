e85d7c8573f69d8d3c21992865efcf48f0985d4a
SalvatoreAdalberto <salvatoreadalberto.esposito@gmail.com>
2023-05-13 12:41:01 +0200

    inchannells

--- train_classifier_EMG.py
+++ train_classifier_EMG.py
@@ -161,13 +161,14 @@ def train(action_classifier, train_loader, val_loader, device, num_classes):
         
         for m in modalities:
             print(f'yoyo1: {data[m].size()}, {data[m].shape}')
-            data[m] = data[m].permute(1, 0, 2)
+            data[m] = data[m].reshape(-1,16,5,32,32)
+            data[m] = data[m].permute(2, 0, 1, 3,4 )
             print(f'yoyo2: {data[m].size()}, {data[m].shape}')
         
-            # print(f"Data after permutation: {data[m].size()}")
         for i_c in range(args.test.num_clips):
             for m in modalities:
                 # extract the clip related to the modality
+                print(i_c, data[m][i_c].shape)
                 clip = data[m][i_c].to(device)
         # in case of multi-clip training one clip per time is processed
         for m in modalities:
