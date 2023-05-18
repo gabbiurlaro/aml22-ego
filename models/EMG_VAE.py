a7c355e375082b0876730dc01fc52a2b8d53db80
SalvatoreAdalberto <salvatoreadalberto.esposito@gmail.com>
2023-05-18 21:05:33 +0200

    vae_emg_tests

--- models/EMG_VAE.py
+++ models/EMG_VAE.py
@@ -14,16 +14,10 @@ class VariationalEncoder(nn.Module):
 
         self.encoder = nn.Sequential(
             nn.Conv2d(in_channels, 32, kernel_size=4, stride=2, padding=1),
-            nn.BatchNorm2d(32),
             
             nn.Conv2d(32, 64, kernel_size=4, stride=2, padding=1),
-            nn.BatchNorm2d(64),
-            
             nn.Conv2d(64, 128, kernel_size=4, stride=2, padding=1),
-            nn.BatchNorm2d(128),
-            
             nn.Conv2d(128, 256, kernel_size=4, stride=2, padding=1),
-            nn.BatchNorm2d(256),
             
             nn.Conv2d(256, latent_dims, kernel_size=4, stride=2, padding=1),
             
