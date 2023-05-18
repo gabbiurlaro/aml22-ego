18bf4f3154e15239aa228cb0232c9cb2af584d9d
SalvatoreAdalberto <salvatoreadalberto.esposito@gmail.com>
2023-05-18 17:56:38 +0200

    vae_emg_tests

--- models/__init__.py
+++ models/__init__.py
@@ -4,6 +4,5 @@ from models.I3D import I3D
 from models.FinalClassifier import Classifier, MLP_late_fusion, action_TRN
 from models.actionLSTM import ActionLSTM 
 from models.FC_VAE import VariationalAutoencoder as VAE
-from models.VAE import VAE1
 from models.EMG_classifier import EMG_classifier
 
