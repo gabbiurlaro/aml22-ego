9bfd95664bf1ad03e3746c9023e12cd39f5592bc
SalvatoreAdalberto <salvatoreadalberto.esposito@gmail.com>
2023-05-22 10:06:55 +0200

    pretrained feature extractor EMG

--- train_classifier_EMG.py
+++ train_classifier_EMG.py
@@ -211,8 +211,7 @@ def save_feat(model, loader, device, it, num_classes, train=False):
             
                 output, feat = model(data)
                 logits[m] = output[m]
-                logger.info(f'feat: {len(feat.keys())}, {feat[0][m].shape}')
-                exit(-1)
+                logger.info(f'feat: {len(feat.keys())}, {feat[0][m].shape}, {args.save.num_clips}')
                 features[m] = torch.Tensor([ feat[i][m] for i in range(args.save.num_clips)])
                 logits[m] = torch.mean(logits[m], dim=0) # average over clips to predict the label
            
