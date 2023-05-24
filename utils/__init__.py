2f715f52a48b12c4624e23a0d4bed865c0c4e2a7
SalvatoreAdalberto <salvatoreadalberto.esposito@gmail.com>
2023-05-24 13:58:29 +0200

    pretrained feature extractor EMG

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,9 +1,2 @@
-from actionnet_record import *
-from args import *
-from epic_record import *
-from logger import *
-from loaders import *
-from video_record import *
-from utils import *
-from transforms import *
+
 
