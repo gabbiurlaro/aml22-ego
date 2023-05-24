63872cec28677fabe3d6be329fe02b859336331d
SalvatoreAdalberto <salvatoreadalberto.esposito@gmail.com>
2023-05-24 19:28:59 +0200

    sae

--- utils/loaders.py
+++ utils/loaders.py
@@ -331,7 +331,7 @@ class ActionNetDataset(data.Dataset, ABC):
             pickle_name = split + "_test.pkl"
         
         
-        return dataset_conf.keys()
+        return dataset_conf
         self.list_file = pd.read_pickle(os.path.join(dataset_conf['annotations_path'], pickle_name))
         #print(f'list_val_load: {self.list_file}, add: {os.path.join(self.dataset_conf.annotations_path, pickle_name)}')
         logger.info(f"Dataloader for {split}-{self.mode} with {len(self.list_file)} samples generated")
