d9f4c9938e085f4aae1296207394bfa6dd6d9afb
SalvatoreAdalberto <salvatoreadalberto.esposito@gmail.com>
2023-05-26 16:19:04 +0200

    sae

--- tasks/action_recognition_task.py
+++ tasks/action_recognition_task.py
@@ -43,7 +43,7 @@ class ActionRecognition(tasks.Task, ABC):
                     features[k] = {}
             for k in feat.keys():
                 features[k][m] = feat[k]
-        logger.info(f'features shape: {len(features.keys())}, {features.keys()}, {features[0]["EMG"].shape}\n {features[0]["EMG"]}')
+        logger.info(f'action_rec: features shape: {len(features.keys())}, {features.keys()}, {features[0]["EMG"].shape}\n {features[0]["EMG"]}')
         return logits, features
 
     def compute_loss(self, logits, label, loss_weight=1.0):
--- train_classifier_EMG.py
+++ train_classifier_EMG.py
@@ -245,7 +245,7 @@ def save_feat(model, loader, device, it, num_classes, train=False, aug=None):
             
                 output, feat = model(data)
                 logits[m] = output[m]
-                logger.info(f'feat shape: {len(feat.keys())}, {feat.keys()}, {feat}')
+                logger.info(f'main : feat shape: {len(feat.keys())}, {feat.keys()}, {feat}')
                 exit(-1)
                 swap = [feat[i][m] for i in range(args.save.num_clips)]
 
