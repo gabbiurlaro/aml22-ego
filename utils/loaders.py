fff7b366e966797622214a2b3b85d752cd12f973
SalvatoreAdalberto <salvatoreadalberto.esposito@gmail.com>
2023-05-29 21:45:54 +0200

    sae

--- utils/loaders.py
+++ utils/loaders.py
@@ -520,6 +520,7 @@ class ActionNetDataset(data.Dataset, ABC):
                 for i in range(16):
                     print(f'process_data_i!: {process_data[i].shape}')
                     signal = spectrogram(process_data[i])
+                    print(f'signal!: {signal.shape}')
                     result.append(torch.stack([signal[j] for j in indices]))
                 spectrograms = torch.stack(result)
                 process_data = spectrograms
