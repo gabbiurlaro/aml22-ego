49f8a9e3a59c7d77a452e908c3f8b6b388326a64
SalvatoreAdalberto <salvatoreadalberto.esposito@gmail.com>
2023-05-31 14:49:31 +0200

    sae

--- models/EMG_classifier.py
+++ models/EMG_classifier.py
@@ -10,6 +10,7 @@ class EMG_classifier(nn.Module):
         self.num_input = num_input
         self.num_classes = num_classes
         self.num_clips = num_clips
+        self.emdedding_size = 1024
         self.classifier = nn.Sequential( 
             #16x32x32
             nn.Conv2d(num_input, 32, kernel_size=3, stride=2, padding=1),  # Output size: 32x16x16
@@ -24,10 +25,10 @@ class EMG_classifier(nn.Module):
             nn.Conv2d(128, 256, kernel_size=3, stride=2, padding=1),  # Output size: 256x2x2
             nn.BatchNorm2d(256),  # Apply batch normalization
             nn.ReLU(),  # Apply ReLU activation
-           # nn.Conv2d(256, 1024, kernel_size=3, stride=2, padding=1),# Output size: 1024x1x1
-           # nn.BatchNorm2d(1024),  # Apply batch normalization
-           # nn.ReLU(),  # Apply ReLU activation
-            nn.AdaptiveAvgPool2d((1, 1)),
+            nn.Conv2d(256,  self.emdedding_size , kernel_size=3, stride=2, padding=1),# Output size: 1024x1x1
+            nn.BatchNorm2d( self.emdedding_size ),  # Apply batch normalization
+            nn.ReLU(),  # Apply ReLU activation
+            #nn.AdaptiveAvgPool2d((1, 1)),
             nn.Flatten()  # Output size: 1024x1x1
             # nn.Conv2d(num_input, 32, kernel_size=4, stride=2, padding=1),
             # nn.BatchNorm2d(32),
@@ -45,7 +46,7 @@ class EMG_classifier(nn.Module):
             # nn.MaxPool2d(2, stride=1)
         )
         self.fc = nn.Sequential(
-            nn.Linear(256, 128),
+            nn.Linear( self.emdedding_size , 128),
             nn.ReLU(),
             nn.Linear(128, num_classes)
         )
