7802f8baa3c26067f89b8d8132cc6696de847f01
SalvatoreAdalberto <salvatoreadalberto.esposito@gmail.com>
2023-06-02 18:12:55 +0200

    64f_10c_512o

--- train_classifier_EMG.py
+++ train_classifier_EMG.py
@@ -293,7 +293,7 @@ def main():
                                                     num_workers=args.dataset.workers, pin_memory=True, drop_last=True)
             val_loader = torch.utils.data.DataLoader(ActionNetDataset(args.dataset.shift.split("-")[-1], modalities,
                                                                         'test', args.dataset,  {'EMG':args.train.num_frames_per_clip.EMG}, args.train.num_clips, args.train.dense_sampling,
-                                                                       transform=transform, load_feat=False, kwargs={'require_spectrogram': True}), additional_info=False,
+                                                                       transform=transform, load_feat=False, kwargs={'require_spectrogram': True},  additional_info=False),
                                                     batch_size=args.batch_size, shuffle=True,
                                                     num_workers=args.dataset.workers, pin_memory=True, drop_last=True)
             
