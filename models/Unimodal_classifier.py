7039e40a015e9c05a6f55309d016dc14d2d1d37d
SalvatoreAdalberto <salvatoreadalberto.esposito@gmail.com>
2023-06-11 17:53:03 +0200

    trying harder

--- models/Unimodal_classifier.py
+++ models/Unimodal_classifier.py
@@ -4,7 +4,7 @@ import torch
 from utils.logger import logger
 import numpy as np
 
-class Multimodal_classifier_parametric(nn.Module):
+class Unimodal_classifier_parametric(nn.Module):
     """
     EMG classifier model for action recognition.
     Based on a convolutional neural network and a fully connected classifier.
@@ -26,18 +26,8 @@ class Multimodal_classifier_parametric(nn.Module):
 
 def forward(self, x):
         logits = []
-        feats = []
-        for clip in range(self.num_clips):
-            _, channels, height, width = x[clip].shape
-            
-            if channels != self.input_size[0] or height != self.input_size[1] or width != self.input_size[2]:
-                raise ValueError(f"Input shape {x[clip].shape} does not match the expected shape {self.input_size}")
-            y = self.backbone(x[clip])
-            feats.append(y.squeeze())
-            if self.include_top:
-                logit = self.on_top_classifier(y)
-                logits.append(logit)
-        if self.include_top:
-            return torch.stack(logits, dim=0).mean(dim=0), {i: feats[i] for i in range(self.num_clips)}
-        else:
-            {i: feats[i] for i in range(self.num_clips)}
+        for clip in range(self.num_clips):    
+            logit = self.on_top_classifier(x[clip])
+            logits.append(logit)
+       
+        return torch.stack(logits, dim=0).mean(dim=0), {}
