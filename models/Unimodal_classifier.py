3cccd2950eca63ce29cbba7a434628861a9f344b
SalvatoreAdalberto <salvatoreadalberto.esposito@gmail.com>
2023-06-11 17:39:21 +0200

    multimodal classification attempt 1

--- models/Unimodal_classifier.py
+++ models/Unimodal_classifier.py
@@ -1,7 +1,27 @@
-nn.Linear(fc_input_size, fc_hidden_size),
+
+from torch import nn
+import torch
+from utils.logger import logger
+import numpy as np
+
+class Multimodal_classifier_parametric(nn.Module):
+    """
+    EMG classifier model for action recognition.
+    Based on a convolutional neural network and a fully connected classifier.
+    """
+    def __init__(self, input_size, num_classes, num_clips, **kwargs) -> None:
+        super().__init__()
+       
+        self.input_size = input_size
+        self.num_classes = num_classes
+        self.num_clips = num_clips
+        
+        self.dropout_rate = kwargs.get("dropout_rate", 0.6)
+
+        self.classifier = nn.Sequential(nn.Linear(input_size, int(input_size/4)),
             nn.ReLU(inplace=True),
-            nn.Linear(fc_hidden_size, num_classes),
-            nn.Dropout(self.dropout_rate)
+            nn.Linear(int(input_size/4), num_classes),
+            nn.Dropout(self.dropout_rate))
 
 
 def forward(self, x):
