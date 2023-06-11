922ae52c920e087d998fb20450cb5e3d8c7efab5
SalvatoreAdalberto <salvatoreadalberto.esposito@gmail.com>
2023-06-11 20:02:14 +0200

    trying harder

--- train_classifier_multimodal.py
+++ train_classifier_multimodal.py
@@ -225,8 +225,6 @@ def validate(model, val_loader, device, it, num_classes):
             label = label.to(device)
             #print(f'data: {data.size()}, {data.shape }, label: {label.size()}, {label.shape}')
             for m in modalities:
-                data[m] = data[m].permute(1, 0, 2)
-                print(f'data: {data[m].size()}, {data[m].shape }, label: {label.size()}, {label.shape}')
                 data[m] = data[m].to(device)
 
                 batch = data[m].shape[0]
