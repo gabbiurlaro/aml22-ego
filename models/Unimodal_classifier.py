e6b6088d135eb97493be9fc2fe72d790eda1a04e
gabbiurlaro <iurlaro.gabriele@gmail.com>
2023-06-20 19:23:59 +0200

    Models

--- models/Unimodal_classifier.py
+++ models/Unimodal_classifier.py
@@ -18,10 +18,10 @@ class Unimodal_classifier_parametric(nn.Module):
         
         self.dropout_rate = kwargs.get("dropout_rate", 0.6)
 
-        self.classifier = nn.Sequential(nn.Linear(input_size, int(input_size/2)),
+        self.classifier = nn.Sequential(nn.Linear(input_size, int(input_size/4)),
             nn.ReLU(inplace=True),
-            nn.Linear(int(input_size/2), int(input_size/4)),
-            nn.ReLU(inplace=True),
+            # nn.Linear(int(input_size/1), int(input_size/4)),
+            # nn.ReLU(inplace=True),
             nn.Linear(int(input_size/4), num_classes),
             nn.Dropout(self.dropout_rate))
     
