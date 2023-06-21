c21c1a8bdb06c9b9740f4d1c31458bc04be33bc4
gabbiurlaro <iurlaro.gabriele@gmail.com>
2023-06-21 11:26:09 +0200

    Fixed(?)

---
+++ models/Unimodal_classifier.py
@@ -0,0 +1,34 @@
+
+from torch import nn
+import torch
+from utils.logger import logger
+import numpy as np
+
+class Unimodal_classifier_parametric(nn.Module):
+    """
+    EMG classifier model for action recognition.
+    Based on a convolutional neural network and a fully connected classifier.
+    """
+    def __init__(self, input_size, num_clips, num_classes, **kwargs) -> None:
+        super().__init__()
+       
+        self.input_size = input_size
+        self.num_classes = num_classes
+        self.num_clips = num_clips
+        
+        self.dropout_rate = kwargs.get("dropout_rate", 0.6)
+
+        self.classifier = nn.Sequential(nn.Linear(input_size, int(input_size/4)),
+            nn.ReLU(inplace=True),
+            # nn.Linear(int(input_size/1), int(input_size/4)),
+            # nn.ReLU(inplace=True),
+            nn.Linear(int(input_size/4), num_classes),
+            nn.Dropout(self.dropout_rate))
+    
+    def forward(self, x):
+        logits = []
+        for clip in range(self.num_clips):    
+            logit = self.classifier(x[clip])
+            logits.append(logit)
+        return torch.stack(logits, dim=0).mean(dim=0), {}
+
